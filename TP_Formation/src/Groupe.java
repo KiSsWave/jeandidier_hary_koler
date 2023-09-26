2c8ff2da490a90f5d5c44fd91d21fb0a78dca69d
Clem's <jeandidier.clement@gmail.com>
2023-09-26 09:13:13 +0200

    ajout groupe et test

---
+++ TP_Formation/src/Groupe.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Groupe {
+    private Map<Formation, List<Etudiant>> etudiants;
+
+    public Groupe() {
+        this.etudiants = new HashMap<>();
+    }
+
+    public void ajouterEtudiant(Etudiant etudiant) {
+        Formation formationEtudiant = etudiant.getFormation();
+        List<Etudiant> listeEtudiants = etudiants.get(formationEtudiant);
+        if (listeEtudiants == null) {
+            listeEtudiants = new ArrayList<>();
+            etudiants.put(formationEtudiant, listeEtudiants);
+        }
+        listeEtudiants.add(etudiant);
+    }
+
+    public void supprimerEtudiant(Etudiant etudiant) {
+        Formation formationEtudiant = etudiant.getFormation();
+        List<Etudiant> listeEtudiants = etudiants.get(formationEtudiant);
+        if (listeEtudiants != null) {
+            listeEtudiants.remove(etudiant);
+            if (listeEtudiants.isEmpty()) {
+                etudiants.remove(formationEtudiant);
+            }
+        }
+    }
+
+    public Map<Formation, List<Etudiant>> getEtudiants() {
+        return etudiants;
+    }
+
+}
